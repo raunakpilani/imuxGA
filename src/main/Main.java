677571f8d465524124ab5fe70cacdff002b60dc1
Raunak <Raunak@Raunak-PC>
2011-01-27 18:29:14 +0530

    generating skeleton random graph... adding edges based on connProb remaining.

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,24 +20,57 @@ public class Main {
 		List<Integer> disconnectedVerts = new ArrayList<Integer>(vertNum);
 		List<Integer> connectedVerts = new ArrayList<Integer>(vertNum);
 		Random rnd = new Random();
-		int n1, n2, len;
+		int n1Ind, n2Ind,n1,n2, len;
 		writeToFile(vertNum+"");
 		
 		for (int i = 0; i < vertNum;i++) {
 			 disconnectedVerts.add(i);
 		}
 		
-		n1 = rnd.nextInt(disconnectedVerts.size());
-		connectedVerts.add(disconnectedVerts.get(n1));
-		n2 = rnd.nextInt(disconnectedVerts.size());
-		connectedVerts.add(disconnectedVerts.get(n1));
+		// initially read two random vertices from disconnVerts and add a link between them
+		// first, get a random index of the node
+		// next, add it to connVerts
+		// finally, remove it from disconnVerts
+		
+		n1Ind = rnd.nextInt(disconnectedVerts.size());
+		n1 = disconnectedVerts.get(n1Ind);
+		connectedVerts.add(n1);
+		disconnectedVerts.remove(n1Ind);
+		
+		n2Ind = rnd.nextInt(disconnectedVerts.size());
+		n2 = disconnectedVerts.get(n2Ind);
+		connectedVerts.add(n2);
+		disconnectedVerts.remove(n2);
+		
+		// random length
 		len = rnd.nextInt(maxLenBy10);
 		
 		writeToFile(n1 + " "+ n2+ " "+ len*10+" "+capacity);
+		
 		while (!disconnectedVerts.isEmpty()) {
+			// make sure all nodes are connected
+			// pick one node from the connVerts and other from disconnVerts
+			// add edge
 			
+			n1Ind = rnd.nextInt(connectedVerts.size());
+			n1 = connectedVerts.get(n1Ind);
+			
+			n2Ind = rnd.nextInt(disconnectedVerts.size());
+			n2 = disconnectedVerts.get(n2Ind);
+			connectedVerts.add(n2);
+			disconnectedVerts.remove(n2);
+			
+			// random length
+			len = rnd.nextInt(maxLenBy10);
+			
+			writeToFile(n1 + " "+ n2+ " "+ len*10+" "+capacity);
 		}
 		
+		// add edges to the graph depending on a connection probability
+		// connProb = 0 => graph already generated
+		// connProb = 100 => complete graph
+		// otherwise, a pair of nodes is considered only if they are not directly connected
+		// random number in [0,1] is generated 
 		for (int connProb = 1; connProb < 100; connProb++) {
 			
 		}
@@ -61,3 +94,4 @@ public class Main {
 	}
 
 }
+
